dcf003858696d341264268d803df1157e75d662c
zimt28 <1764689+zimt28@users.noreply.github.com>
2021-01-23 01:19:37 +0100

    Use new Ash helper

--- lib/ash_audit_log/transformers/add_notifier.ex
+++ lib/ash_audit_log/transformers/add_notifier.ex
@@ -2,17 +2,16 @@ defmodule AshAuditLog.Transformers.AddNotifier do
   @moduledoc "Adds the generated audit log to the resource's notifiers"
   use Ash.Dsl.Transformer
 
+  import Ash.Dsl.Transformer, only: [get_persisted: 3, persist: 3]
   import AshAuditLog.Resource, only: [audit_log_module: 1]
-
-  alias Ash.Dsl.Transformer
 
   def before?(Ash.Api.Transformers.EnsureResourcesCompiled), do: true
   def before?(_), do: false
 
   def transform(resource, dsl) do
-    notifiers = [audit_log_module(resource)] ++ get_in(dsl, [:persist, :notifiers])
+    notifiers = [audit_log_module(resource)] ++ get_persisted(dsl, :notifiers, nil)
 
-    dsl = Transformer.persist(dsl, :notifiers, notifiers)
+    dsl = persist(dsl, :notifiers, notifiers)
 
     {:ok, dsl}
   end
